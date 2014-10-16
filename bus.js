8baab25dea20f8b87410c797e9b0b80c014c08e8
James Woglom <hypnotoad@wogloms.com>
2014-10-16 15:44:36 -0400

    stashed code

--- bus.js
+++ bus.js
@@ -15,7 +15,83 @@ function renameBus(busText) {
 decode = function(text) {
     return decodeURIComponent(text).replace(/_/g, ' ');
 }
+function createText(layer, title, itext, moveable) {
+    var group = new Kinetic.Group({
+    	draggable: moveable
+    });
+    var rect = new Kinetic.Rect({
+    	x: 50,
+	y: 50,
+	width: 200,
+	height: 200
+    });
+    var text = new Kinetic.Text({
+    	x: 54,
+	y: 54,
+	text: decode(itext),
+	fill: 'black',
+	opacity: 1.0,
+	width: 200,
+	height: 200,
+	fontSize: 22,
+	fontFamily: 'ComicSans',
+	align: 'center'
+    });
+    var text2 = new Kinetic.Text({
+	x: 54,
+	y: 84,
+	text: decode(title),
+	fill: 'black',
+	opacity: 1.0,
+	width: 200,
+	height: 170,
+	fontSize: 14,
+	fontFamily: 'ComicSans',
+	align: 'center'
+    });
+    group.add(rect);
+    group.add(text);
+    if(moveable) {
+        group.on('dblclick', function() {
+            var p = prompt('Choose one of the following commands:\n\n'+
+                           '\ttitle [new title]\t\tChange title\n'+
+                           '\tdelete\t\t\tDelete this\n'+
+                           '\tmove [x] [y]\t\tMove to position (x,y)\n'+
+                           '\tchanged\t\t\tForce change position\n'+
+                           '\tnuke\t\t\tRemove all buses (WARNING)\n'+
+                           '\nType your choice below..');
+            var c = p.split(' ',1);
+            if(c[0] == 'title' || c[0] == 'delete') {
+                positionsChanged[busName] = true;
+                var opos = positions[busName];
+                moveBus(busName, -999, -999);
+                if(p != 'delete') {
+                    createBus(layer, c[1], moveable);
+                    moveBus(c[1], opos[0], opos[1]);
+                }
+            } else if(c[0] == 'move') {
+                var mv = p.split(' ');
+                moveBus(busName, mv[1], mv[2]);
+                positionsChanged[busName] = true;
+            } else if(c[0] == 'changed') {
+                positionsChanged[busName] = true;
+            } else if(c[0] == 'nuke') {
+                nuke();
+            }
+        });
+        group.on('dragmove', function() {
+            console.log(group.getAbsolutePosition().x+', '+group.getAbsolutePosition().y);
+            positions[busName] = [group.getAbsolutePosition().x, group.getAbsolutePosition().y];
+            positionsChanged[busName] = true;
+            window.saved = false; unsavedAnim();
+        });
+    }
+    layer.add(group);
+    group.draw();
+}
 function createBus(layer, busName, moveable) {
+    var textbox = (busName != null && busName.substring(0,5) == 'text:');
+    var tbn = busName.substring(5);
     console.log('createBus '+busName);
     if(typeof moveable == 'undefined') moveable = true;
     var group = new Kinetic.Group({
@@ -24,23 +100,23 @@ function createBus(layer, busName, moveable) {
     var rect = new Kinetic.Rect({
         x: 50,
         y: 50,
-        width: 40,
-        height: 200,
+        width: textbox ? 200 : 40,
+        height: textbox ? 200 : 200,
         fill: 'yellow',
         stroke: 'yellow',
         strokeWidth: 2
     });
     var text = new Kinetic.Text({
         x: 54,
-        y: 250,
-        text: decode(busName),
-        rotationDeg: 270,
+        y: textbox ? 50 : 250,
+        text: decode(textbox ? tbn : busName),
+        rotationDeg: textbox ? 0 : 270,
         fill: 'black',
-        stroke: 'black',
+        stroke: textbox ? '' : 'black',
         opacity: 1.0,
-        width: 200,
-        height: 40,
-        fontSize: 32,
+        width: textbox ? 200 : 200,
+        height: textbox ? 200 : 40,
+        fontSize: textbox ? 14 : 32,
         fontFamily: 'ComicSans',
         align: 'center'
     });
@@ -81,7 +157,6 @@ function createBus(layer, busName, moveable) {
             positions[busName] = [group.getAbsolutePosition().x, group.getAbsolutePosition().y];
             positionsChanged[busName] = true;
             window.saved = false; unsavedAnim();
-
         });
     }
     
